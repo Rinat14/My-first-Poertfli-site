eafb5da2dbe7ec3cb5c0d7b6eb5bd95e91975ced
Rinat Samandarov <rinatsamandarov14@gmail.com>
2022-12-21 14:30:38 +0500

    The home page build and styles in SCSS

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const lightModeBtn = document.querySelector('.light-mode-button__btn');
+
+(function() {
+	lightModeBtn.addEventListener('click', () => {
+		document.body.classList.toggle('light-mode');
+		document.body.classList.toggle('dark-mode');
+		lightModeBtn.classList.toggle('active');
+		lightModeBtn.parentElement.classList.toggle('active');
+	});
+})();

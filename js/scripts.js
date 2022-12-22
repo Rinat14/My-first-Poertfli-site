42a3361b9dfde0abcb1eb44bf166ef3a5f53bfdc
Rinat Samandarov <rinatsamandarov14@gmail.com>
2022-12-22 15:44:30 +0500

    Delete SCSS files and make change color mode function

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ const lightModeBtn = document.querySelector('.light-mode-button__btn');
 
 (function() {
 	lightModeBtn.addEventListener('click', () => {
-		document.body.classList.toggle('light-mode');
 		document.body.classList.toggle('dark-mode');
 		lightModeBtn.classList.toggle('active');
 		lightModeBtn.parentElement.classList.toggle('active');
